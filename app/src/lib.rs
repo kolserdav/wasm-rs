b9b729fb9974150e4c2d2143230003b1f9391cb1
kolserdav <serega12101983@gmail.com>
2023-02-11 01:15:06 +0700

    conect features

--- app/src/lib.rs
+++ app/src/lib.rs
@@ -1,7 +1,21 @@
 use wasm_bindgen::prelude::*;
+extern crate web_sys;
+use web_sys::{Document, Window};
 
 #[wasm_bindgen]
 pub fn say(s: String) -> String {
     let r = String::from("hello ");
     return r + &s;
+}
+
+#[wasm_bindgen]
+pub fn say_hello() {
+    let window = web_sys::window().expect("Error get window");
+    let document = window.document().expect("Error get document");
+    let body = document
+        .query_selector("body")
+        .expect("body not found")
+        .unwrap();
+    let hello: Option<&str> = Some("Hello Webassembly!");
+    body.set_text_content(hello);
 }
--- src/index.js
+++ src/index.js
@@ -1,7 +1,8 @@
-import init, { say } from '../app/pkg/wasm_app.js';
+import init, { say, say_hello } from '../app/pkg/wasm_app.js';
 
 window.onload = async () => {
   await init();
   const res = say('Test');
   console.log(res);
+  say_hello();
 };
