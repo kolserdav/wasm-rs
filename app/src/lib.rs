7161b57aed7764c3883fe0716179497741662231
kolserdav <serega12101983@gmail.com>
2023-02-11 12:02:15 +0700

    try gloo

--- app/src/lib.rs
+++ app/src/lib.rs
@@ -1,8 +1,10 @@
 use wasm_bindgen::prelude::*;
 extern crate web_sys;
-use web_sys::console;
 mod prelude;
+use gloo_utils::format::JsValueSerdeExt;
 use prelude::{el, Error, ErrorKind};
+use serde::{Deserialize, Serialize};
+use serde_wasm_bindgen::to_value;
 
 #[wasm_bindgen]
 pub fn say(s: String) -> String {
@@ -10,14 +12,35 @@ pub fn say(s: String) -> String {
     return r + &s;
 }
 
+#[derive(Serialize, Deserialize)]
+pub struct Res {
+    first: u8,
+    second: bool,
+}
+
 #[wasm_bindgen]
 pub fn say_hello() -> Result<JsValue, JsError> {
-    let body_o = el("b2ody");
+    let body_o = el("body");
     if let Err(e) = body_o {
-        return Err(JsError::from(Error::new(ErrorKind::NotFound, "err")));
+        return Err(JsError::from(Error::new(
+            ErrorKind::NotFound,
+            format!("Error: {:?}", e),
+        )));
     }
     let body = body_o.unwrap();
     let hello: Option<&str> = Some("Hello Webassembly!");
     body.set_text_content(hello);
-    Ok(JsValue::from("sucess"))
+    let data = Res {
+        first: 244,
+        second: true,
+    };
+    let res_r = JsValue::from_serde(&data);
+    if let Err(e) = res_r {
+        return Err(JsError::from(Error::new(
+            ErrorKind::Interrupted,
+            format!("Error: {:?}", e),
+        )));
+    }
+    let res = res_r.unwrap();
+    Ok(res)
 }
